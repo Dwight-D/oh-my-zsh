3157a982c21df75da4abb4348b4f1929065b23e8
Max Forsman <max.forsman@sinch.com>
2019-06-07 10:43:13 +0200

    Moved configs from dotfile to this repo

--- binds.zsh
+++ binds.zsh
@@ -1,1 +1,11 @@
-../../dotfiles/oh-my-zsh/.oh-my-zsh/custom/binds.zsh
+zle -N copy-earlier-word
+bindkey '^[lastarg' copy-earlier-word
+
+
+#Arrow key history search
+autoload -U up-line-or-beginning-search
+autoload -U down-line-or-beginning-search
+zle -N up-line-or-beginning-search
+zle -N down-line-or-beginning-search
+bindkey '^[[A' up-line-or-beginning-search # Up
+bindkey '^[[B' down-line-or-beginning-search # Down
--- functions.zsh
+++ functions.zsh
@@ -1,1 +1,55 @@
-../../dotfiles/oh-my-zsh/.oh-my-zsh/custom/functions.zsh
+#cd to dir and ls contents
+function cd() {
+    if [ -z "$*" ]; then
+        destination=~
+    else
+        destination=$*
+    fi
+    builtin cd "${destination}" >/dev/null && ls
+}
+
+#Plays a sound depending on result of previous command (passed as pos arg)
+ping (){
+    result=$1
+    #If last command was a failure, set to fail sound
+    if [ "$result" != 0 ]; then
+        sound=/System/Library/Sounds/Basso.aiff    
+    else
+        sound=/System/Library/Sounds/Ping.aiff
+    fi
+    afplay "$sound" -v 4
+}
+
+#Perform pos arg as command and notifies with sound and notification when completed
+#based on command result
+notify (){
+    #Perform argument as command
+    eval $@
+    result=$?
+    #Play sound
+    ping $result
+    cmd_notification "$@" $result
+}
+
+#Display a notification based on command and its result (pos args)
+cmd_notification (){
+     if [[ "$#" -lt 2 ]]; then
+	echo "Usage: cmd_notification result command"
+	return
+     fi
+     result=$1
+     shift
+     cmd=$@
+     notify_text="Command completed: "
+     if [[ "$result" != 0 ]]; then
+         notify_text+="FAILURE - $result"
+     else
+         notify_text+=SUCCESS
+     fi
+     osascript -e "display notification \"$notify_text\" with title \"$cmd\""
+}
+
+#Read markdown in cli browser
+rmd () {
+  pandoc $1 | lynx -stdin
+}

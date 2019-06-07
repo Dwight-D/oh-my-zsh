80c7e95666916db161f5a9141cb83d29cd4e9293
Max Forsman <max.forsman@sinch.com>
2019-06-07 11:04:27 +0200

    Fixed arrow key history search

--- binds.zsh
+++ binds.zsh
@@ -1,11 +1,25 @@
-zle -N copy-earlier-word
-bindkey '^[lastarg' copy-earlier-word
+autoload -Uz insert-last-word 
+zle -N insert-last-word 
+bindkey -M viins '^[lastarg' insert-last-word
 
 
 #Arrow key history search
-autoload -U up-line-or-beginning-search
-autoload -U down-line-or-beginning-search
-zle -N up-line-or-beginning-search
-zle -N down-line-or-beginning-search
-bindkey '^[[A' up-line-or-beginning-search # Up
-bindkey '^[[B' down-line-or-beginning-search # Down
+#autoload -Uz up-line-or-beginning-search
+#autoload -Uz down-line-or-beginning-search
+#zle -N up-line-or-beginning-search
+#zle -N down-line-or-beginning-search
+#bindkey -M viins '^[[A' up-line-or-beginning-search 
+#bindkey -M viins '^[[B' down-line-or-beginning-search 
+
+# start typing + [Up-Arrow] - fuzzy find history forward
+if [[ "${terminfo[kcuu1]}" != "" ]]; then
+  autoload -U up-line-or-beginning-search
+  zle -N up-line-or-beginning-search
+  bindkey -M viins "${terminfo[kcuu1]}" up-line-or-beginning-search
+fi
+# start typing + [Down-Arrow] - fuzzy find history backward
+if [[ "${terminfo[kcud1]}" != "" ]]; then
+  autoload -U down-line-or-beginning-search
+  zle -N down-line-or-beginning-search
+  bindkey -M viins "${terminfo[kcud1]}" down-line-or-beginning-search
+fi

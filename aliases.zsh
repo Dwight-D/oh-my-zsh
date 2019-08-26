3731f3775092a068cc57c4bcbd09326b113990d7
Max Forsman <max.forsman@sinch.com>
2019-08-26 08:37:33 +0200

    Updating

--- aliases.zsh
+++ aliases.zsh
@@ -3,3 +3,5 @@ alias gradlew='./gradlew'
 alias rr='ranger-cd'
 alias ssh='backpack'
 alias shrug="printf '¯\_(ツ)_/¯' | pbcopy"
+alias sdiff='sdiff --diff-program=colordiff -w $(($(tput cols) - 10))'
+alias shreload='source $HOME/.zshrc'
--- env.zsh
+++ env.zsh
@@ -1,4 +1,4 @@
 export CODE_DIR=/Users/maxfor/code
 export EDITOR=vim
 export PATH=$PATH:$HOME/scripts
-export LESS='-SR'
+export LESS='-SRXF'

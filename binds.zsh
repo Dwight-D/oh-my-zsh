4da78a27aaa1af7f936567ea07172180613d161f
Max <forsman.max@gmail.com>
2021-09-27 20:39:56 +0200

    Enable arrow key search

--- binds.zsh
+++ binds.zsh
@@ -21,20 +21,20 @@ bindkey '^K' kill-line
 #bindkey -M viins '^[[B' down-line-or-beginning-search 
 
 # start typing + [Up-Arrow] - fuzzy find history forward
-#if [[ "${terminfo[kcuu1]}" != "" ]]; then
-#  autoload -U up-line-or-beginning-search
-#  autoload -U up-line
-#  zle -N up-line-or-beginning-search
-#  bindkey -M viins "${terminfo[kcuu1]}" up-line-or-beginning-search
-#  bindkey -M vicmd "${terminfo[kcuu1]}" up-line
-#fi
+if [[ "${terminfo[kcuu1]}" != "" ]]; then
+  autoload -U up-line-or-beginning-search
+  autoload -U up-line
+  zle -N up-line-or-beginning-search
+  bindkey -M viins "${terminfo[kcuu1]}" up-line-or-beginning-search
+  bindkey -M vicmd "${terminfo[kcuu1]}" up-line
+fi
 ## start typing + [Down-Arrow] - fuzzy find history backward
-#if [[ "${terminfo[kcud1]}" != "" ]]; then
-#  autoload -U down-line-or-beginning-search
-#  autoload -U down-line
-#  zle -N down-line-or-beginning-search
-#  bindkey -M viins "${terminfo[kcud1]}" down-line-or-beginning-search
-#  bindkey -M vicmd "${terminfo[kcud1]}" down-line
-#fi
+if [[ "${terminfo[kcud1]}" != "" ]]; then
+  autoload -U down-line-or-beginning-search
+  autoload -U down-line
+  zle -N down-line-or-beginning-search
+  bindkey -M viins "${terminfo[kcud1]}" down-line-or-beginning-search
+  bindkey -M vicmd "${terminfo[kcud1]}" down-line
+fi
 
 export KEYTIMEOUT=1

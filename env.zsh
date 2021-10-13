bc6599e11e71490c85a9c8dab65f0ba44155e87e
Max <forsman.max@gmail.com>
2021-10-13 10:25:39 +0200

    Add spotifycli alias and pip bin dir to path

--- env.zsh
+++ env.zsh
@@ -1,7 +1,7 @@
 export CODE_DIR=$HOME/code
 export NOTES_DIR=$HOME/files/notes
 export EDITOR=vim
-export PATH=$PATH:$HOME/scripts:$HOME/scripts/bashscripts:$HOME/opt/flutter/bin
+export PATH=$PATH:$HOME/scripts:$HOME/scripts/bashscripts:$HOME/opt/flutter/bin:$HOME/.local/bin
 export LESS='-SRXF'
 export LIQUIBASE_HOME=/usr/local/opt/liquibase/libexec
 export LANG="en_US.UTF-8"

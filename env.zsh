1d3d13642a8e13367ca3b6c20ac734a3b10399ac
Max Forsman <forsman.max@protonmail.com>
2022-05-22 12:48:26 +0200

    Update env

--- env.zsh
+++ env.zsh
@@ -1,7 +1,8 @@
 export CODE_DIR=/Users/maxfor/code
 export NOTES_DIR=$HOME/files/notes
 export EDITOR=vim
-export PATH=$PATH:$HOME/scripts
+export BACKPACK_CONF="$HOME/dotfiles/backpack.conf"
+export PATH=$PATH:$HOME/scripts:$HOME/.local/bin
 export LESS='-SRXF'
 export LIQUIBASE_HOME=/usr/local/opt/liquibase/libexec
 export LANG=en_US
